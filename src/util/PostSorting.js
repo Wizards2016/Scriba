063c062b8506702ccdfee25149a524f41d155cdb
Cai Lu <caillu@uw.edu>
2017-01-14 14:36:25 -0800

    Add util that calculates user stats: info by votes or by category

--- src/util/PostSorting.js
+++ src/util/PostSorting.js
@@ -1,20 +1,29 @@
 const earthRadius = 3959; // in miles
 
+// Convert degrees to radians
+const _degreesToRad = (degree) => {
+  return degree * (Math.PI / 180);
+};
+
 // Calculates distance based on the Haversine formula
-const sortByDistance = function(currentLocation, posts) {
+const sortByDistance = (currentLocation, posts) => {
   posts.sort((post1, post2) => {
     // calculate distance between post1 and currentLocation
     const latDistance1 = _degreesToRad(post1.latitude - currentLocation.latitude);
     const longDistance1 = _degreesToRad(post1.longitude - currentLocation.longitude);
-    const a1 = Math.pow(Math.sin(latDistance1 / 2), 2);
-    const b1 = Math.cos(currentLocation.latitude) * Math.cos(post1.latitude) * Math.pow(Math.sin(longDistance1 / 2), 2);
+    const a1 = (Math.sin(latDistance1 / 2) ** 2);
+    const b1 = Math.cos(currentLocation.latitude) *
+              Math.cos(post1.latitude) *
+              (Math.sin(longDistance1 / 2) ** 2);
     const d1 = 2 * earthRadius * Math.sqrt(a1 + b1);
 
     // calculate distance between post2 and currentLocation
     const latDistance2 = _degreesToRad(post2.latitude - currentLocation.latitude);
     const longDistance2 = _degreesToRad(post2.latitude - currentLocation.latitude);
-    const a2 = Math.pow(Math.sin(latDistance2 / 2), 2);
-    const b2 = Math.cos(currentLocation.latitude) * Math.cos(post2.latitude) * Math.pow(Math.sin(longDistance2 / 2), 2);
+    const a2 = (Math.sin(latDistance2 / 2) ** 2);
+    const b2 = Math.cos(currentLocation.latitude) *
+              Math.cos(post2.latitude) *
+              (Math.sin(longDistance2 / 2) ** 2);
     const d2 = 2 * earthRadius * Math.sqrt(a2 + b2);
 
     // Sort in ascending order (closest to furthest from the user)
@@ -25,25 +34,34 @@ const sortByDistance = function(currentLocation, posts) {
 };
 
 // Sorts from newest to oldest
-const sortByTime = function(posts) {
+const sortByTime = (posts) => {
   posts.sort((post1, post2) => {
     return new Date(post2.createdAt) - new Date(post1.createdAt);
   });
   return posts;
 };
 
-// Convert degrees to radians
-const _degreesToRad = function(degree) {
-  return degree * (Math.PI / 180);
-};
-
-const sortByVotes = function(posts) {
+const sortByVotes = (posts) => {
   posts.sort((post1, post2) => {
     return (post2.upVotes - post2.downVotes) - (post1.upVotes - post1.downVotes);
   });
   return posts;
-}
+};
+
+// Returns an object with arrays as values
+const sortByCategory = (posts) => {
+  const sortedPosts = {};
+
+  posts.forEach((post) => {
+    const category = post.category;
+    sortedPosts[category] = sortedPosts[category] || [];
+    sortedPosts[category].push(post);
+  });
+
+  return sortedPosts;
+};
 
 exports.sortByDistance = sortByDistance;
 exports.sortByTime = sortByTime;
 exports.sortByVotes = sortByVotes;
+exports.sortByCategory = sortByCategory;
---
+++ src/util/UserStats.js
@@ -0,0 +1,68 @@
+const API = require('./APIService');
+const PostSorting = require('./PostSorting');
+
+const UserStats = (displayName, userAuth) => {
+  this.displayName = displayName;
+  this.userAuth = userAuth;
+
+  this.messages = undefined;
+  this.votes = undefined;
+  this.user = undefined;
+
+  // Grab data from the database and store for later use
+  API.get.message({
+    displayName: this.displayName
+  })
+  .then((messages) => {
+    this.messages = messages;
+  });
+
+  API.get.vote({
+    displayName: this.displayName
+  })
+  .then((votes) => {
+    this.votes = votes;
+  });
+
+  API.get.user({
+    displayName: this.displayName,
+    userAuth: this.userAuth
+  })
+  .then((user) => {
+    this.user = user;
+  });
+};
+
+UserStats.prototype.getVoteStats = () => {
+  // Number of upvotes,
+  // Number of downvotes
+  // All posts sorted by votes
+  return {
+    upVotes: this.user.upVotes,
+    downVotes: this.user.downVotes,
+    popular: PostSorting.sortByVotes(this.messages)
+  };
+};
+
+UserStats.prototype.getPostStats = () => {
+  // Sort the messages by category
+  const postsByCateory = PostSorting.sortByCategory(this.messages);
+  // Sort the messages by number of upvotes
+  for (const key in postsByCateory) {
+    postsByCateory[key] = PostSorting.sortByVotes(postsByCateory[key]);
+  }
+  // Get the number of posts per category
+  const totalByCategory = {};
+  for (const key in postsByCateory) {
+    totalByCategory[key] = postsByCateory[key].length;
+  }
+
+  // Total number of posts by this user
+  // Number of posts per category
+  // Posts sorted by categories, then by upvotes
+  return {
+    totalPosts: this.user.totalPosts,
+    totalByCategory: totalByCategory,
+    popularByCategory: postsByCateory
+  };
+};
